07df9ecdfddb01822c70cb9ac437ff63f67faa09
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-02-14 11:45:59 -0300

    add our firsts reducers for counter dashboard

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,5 +1,7 @@
 'use client';
-import { useState } from "react"
+import { useAppDispatch, useAppSelector } from "@/store";
+import { addOne, substractOne } from "@/store/counter/counterSlice";
+// import { useState } from "react";
 
 interface Props {
     value?: number;
@@ -7,7 +9,9 @@ interface Props {
 
 export const CartCounter = ({value = 0}: Props) => {
 
-    const [count, setCount] = useState(value);
+    const count = useAppSelector(state => state.counter.count); //obtenemos el valor del state
+    const dispatch = useAppDispatch(); //despachamos acciones
+    // const [count, setCount] = useState(value);
 
     return (
         <>
@@ -15,13 +19,15 @@ export const CartCounter = ({value = 0}: Props) => {
 
             <div className="flex">
                 <button 
-                onClick={() => setCount(count + 1)}
+                // onClick={() => setCount(count + 1)}
+                onClick={() => dispatch( addOne() )}
                 className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
                 +1
                 </button>
 
                 <button 
-                onClick={() => setCount(count - 1)}
+                // onClick={() => setCount(count - 1)}
+                onClick={() => dispatch( substractOne() )}
                 className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
                 -1
                 </button>
